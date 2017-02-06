73f205d61668c76aad9c6ad600c8c8259d15ead2
Zack Snyder <zsnyder317@yahoo.com>
2017-02-06 09:58:34 -0600

    Initial Commit

---
+++ UNITE proto/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UNITE proto
+//
+//  Created by Zack Snyder on 2/6/17.
+//  Copyright © 2017 Zack Snyder. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
